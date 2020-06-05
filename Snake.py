1cfb87ce9fca0caab7e3870059b50379474325f2
Samanien <enrique.joel.rodriguez@fing.edu.uy>
2020-06-05 00:21:02 -0300

    changes and first background

--- Snake.py
+++ Snake.py
@@ -5,18 +5,23 @@ import sys
 #Constants
 FPS = 10
 FPSCLOCK = py.time.Clock()
+py.font.init()
+
 screenX = 600
 screenY = 600
 blockSize = 30
 squareX = int(600/blockSize) #how many squares in each line
-screen = py.display.set_mode((screenX,screenY))
 
-def main():
-    #load logo and set window name
-    logo = py.image.load("./Icons/snake64.png")
-    py.display.set_icon(logo)
-    py.display.set_caption("Snake")
-    
+#load logo and set window name and background
+logo = py.image.load("./Icons/snake64.png")
+py.display.set_icon(logo)
+py.display.set_caption("Snake Game")
+screen = py.display.set_mode((screenX,screenY))
+background = py.image.load("./background.png")
+screen.blit(background,(0,0))
+
+def game():    
+    screen.blit(background,(0,0))
     #starting snake Coord 
     startX = random.randint(3,squareX-5)
     startY = random.randint(1,squareX)
@@ -28,7 +33,6 @@ def main():
     #starting points
     global points
     points = 0
-    py.font.init()
 
     #main loop
     direction = 'right'
@@ -49,6 +53,7 @@ def main():
                 if event.key == py.K_RIGHT:
                     direction = 'right'
         sCoord = movement(direction,sCoord) #snake coordinates
+        screen.blit(background,(0,0))
         drawPoints(points)
         drawApple(apple,sCoord)
         drawSnake(sCoord)
@@ -61,6 +66,47 @@ def main():
         py.display.update()
         if(points == 400):
             running = False
+
+def menu():
+    myfont = py.font.SysFont('Comic Sans MS', 80)
+    text = myfont.render('SnakePY!', False, (255,150,20))
+    myfont2 = py.font.SysFont('Comic Sans MS', 50)
+    text2 = myfont2.render('Enter to play', False, (255,250,20))
+    
+    #butt0 = py.Rect(20,0,300,150)
+    #py.draw.rect(screen,(255,0,0),butt0)
+
+    #main loop
+    running = True
+    while running: 
+        FPSCLOCK.tick(FPS)
+        py.display.update()
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                running = False
+            if event.type == py.KEYDOWN:
+                if event.key == py.K_DOWN: #use up and down for selecting play/options
+                    pass
+                if event.key == py.K_UP:
+                    pass
+                if event.key == py.K_RETURN:
+                    running = False
+                    game()
+        screen.blit(text,(50,30))
+        screen.blit(text2,(50,85))
+
+#colisions
+def colide(snakeCoord):
+    head = snakeCoord[0]
+    x = head['x']
+    y = head['y']
+    if(x > 19 or y > 19 or x < 0 or y < 0):
+        py.quit()
+        exit()
+    for i in range(len(snakeCoord)-1):
+        if (head == snakeCoord[i+1]):
+            py.quit()
+            exit()
 
 def drawGrid():
     for x in range(screenX):
@@ -84,7 +130,7 @@ def drawApple(pos,snakeCoord):
 def drawPoints(points):
     myfont = py.font.SysFont('Comic Sans MS', 30)
     text = myfont.render('Points: ' + str(points), False, (255,255,0))
-    fixBlock({'x':19,'y':0})
+    #fixBlock({'x':19,'y':0})
     screen.blit(text,(500,10))
 
 #control points and apple spawn
@@ -93,20 +139,9 @@ def point(apple,snakeCoord):
     if(apple == snakeCoord[0]):
         points = points + 1
         snakeCoord.insert(0,apple)
-        fixBlock(apple)
+        #fixBlock(apple)
         apple = getRandom()
     return apple
-
-#colisions
-def colide(snakeCoord):
-    head = snakeCoord[0]
-    x = head['x']
-    y = head['y']
-    if(x > 19 or y > 19 or x < 0 or y < 0):
-        py.quit()
-    for i in range(len(snakeCoord)-1):
-        if (head == snakeCoord[i+1]):
-            py,quit()
    
 #control movement
 def movement(dir,snakeCoord):
@@ -118,7 +153,7 @@ def movement(dir,snakeCoord):
     py.draw.rect(screen,(0,0,0),tailrect)
 
     #reDraw the grid where the tail was
-    fixBlock(tail)
+    #fixBlock(tail)
 
     if(dir == 'up'):
         snakeCoord.insert(0,{'x':x,'y':y-1})
@@ -130,17 +165,18 @@ def movement(dir,snakeCoord):
         snakeCoord.insert(0,{'x':x+1,'y':y})
     return snakeCoord
 
-def fixBlock(pos):
-    rect = py.Rect(pos['x']*blockSize,pos['y']*blockSize,blockSize,blockSize)
-    py.draw.rect(screen,(0, 0, 0),rect)
-    rect2 = py.Rect(pos['x']*blockSize,pos['y']*blockSize,blockSize,blockSize)
-    py.draw.rect(screen,(200, 200, 200),rect2,1)
-
 #get random Location
 def getRandom():
     x = random.randint(0,squareX-1)
     y = random.randint(0,squareX-1)
     return {'x':x,'y':y} #returns the pair x y
 
-#run main 
-main()
+def fixBlock(pos):
+    pass
+    #rect = py.Rect(pos['x']*blockSize,pos['y']*blockSize,blockSize,blockSize)
+    #py.draw.rect(screen,(0, 0, 0),rect)
+    #rect2 = py.Rect(pos['x']*blockSize,pos['y']*blockSize,blockSize,blockSize)
+    #py.draw.rect(screen,(200, 200, 200),rect2,1)
+
+#start
+menu()
